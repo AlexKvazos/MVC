56acd151b94d98e4ef10869165953e990a3c7fae
AlexKvazos <contact@alexkvazos.com>
2015-06-06 01:52:20 -0500

    Fixed watch task, ignore bower components, change bower component location, removed eslint globals

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,4 +1,4 @@
 module.exports = {
-  files: ['core/client/**/*.js', 'core/client/**/*.scss'],
+  files: ['core/client/**/*.js', 'core/client/**/*.scss', 'core/client/**/*.html'],
   tasks: ['eslint:client', 'clean:dist', 'browserify:dev', 'copy:assets', 'clean:cache']
 };
