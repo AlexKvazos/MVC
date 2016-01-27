9bba914fa65c87cc1632ab5e67dc94f2f34f51c8
AlexKvazos <contact@alexkvazos.com>
2016-01-26 21:14:50 -0600

    Expose public folders

--- src/server/bootstrap/router.js
+++ src/server/bootstrap/router.js
@@ -4,9 +4,18 @@
  * @description Points application routes to controllers
  */
 
+import { join } from 'path';
+import express from 'express';
 import controllers from '../controllers';
 
+let location = (l) => express.static(join(__dirname, l));
+
 export default (app) => {
+
+  app.use('/public', location('../../../public'));
+  app.use('/public/img', location('../../client/img'));
+  app.use('/public/fonts', location('../../client/fonts'));
+  app.use('/public/templates', location('../../client/templates'));
 
   app.use('/', controllers.MainController);
 
